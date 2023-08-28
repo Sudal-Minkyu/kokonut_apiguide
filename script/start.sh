909021bab21f29ec7750d77c55c0f33a80215891
Woody_Cha <gkstls2006@naver.com>
2023-08-28 22:56:48 +0900

    API 가이드 1.0.1버전 마무리 커밋2

--- script/start.sh
+++ script/start.sh
@@ -21,6 +21,6 @@ fi
 
 mkdir /root/kokonut_api_guide/logs
 
-nohup npm run dev -- --host 0.0.0.0 1>/root/kokonut_api_guide/logs/$(date +%Y-%m-%d)_stdout.log 2>/root/kokonut_api_guide/logs/$(date +%Y-%m-%d)_stderr.log &
+nohup npm run dev
 
 exit
