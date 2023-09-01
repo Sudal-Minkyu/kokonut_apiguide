0052e642048c0e429227463c214d24b50b853f04
Woody_Cha <gkstls2006@naver.com>
2023-09-01 14:41:42 +0900

    쉡스크립트 수정2

--- script/start.sh
+++ script/start.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
+
 sudo su -
 cd /root/kokonut_api_guide/
 
-PORT=8060
-echo "프로세스 종료용 포트조회 : $PORT"
+PID=$(pgrep -f npm)
+kill $PID
 
-PID=$(lsof -i :$PORT -t)
-
-if [ -z "$PID" ]; then
-    echo "종료할 프로세스가 없습니다. $PORT"
-else
-    echo "해당 프로세스를 종료합니다. $PORT is: $PID"
-    kill -9 $PID
-    sleep 5
-fi
+sleep 5
 
 mkdir /root/kokonut_api_guide/logs
 nohup npm run dev -- --host 0.0.0.0 1>/root/kokonut_api_guide/logs/$(date +%Y-%m-%d)_stdout.log 2>/root/kokonut_api_guide/logs/$(date +%Y-%m-%d)_stderr.log &
