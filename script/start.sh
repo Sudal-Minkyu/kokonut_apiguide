209f4ebc6eed5e82ff86f99e819f297b219ca021
Woody_Cha <gkstls2006@naver.com>
2023-09-01 17:33:30 +0900

    쉘스크립트 수정5

--- script/start.sh
+++ script/start.sh
@@ -3,12 +3,19 @@
 sudo su -
 cd /root/kokonut_api_guide/
 
-PID=$(pgrep -f npm)
-kill $PID
+PORT=8060
 
-sleep 5
+PID=$(lsof -i :$PORT -t)
+
+if [ -z "$PID" ]; then
+    echo "$PORT번 으로 가동중인 프로젝트가 없습니다."
+else
+    echo "$PORT번 PID 값: $PID"
+    kill -9 $PID
+    sleep 5
+fi
 
 mkdir /root/kokonut_api_guide/logs
-nohup npm run dev -- --host 0.0.0.0 1>/root/kokonut_api_guide/logs/$(date +%Y-%m-%d)_stdout.log 2>/root/kokonut_api_guide/logs/$(date +%Y-%m-%d)_stderr.log &
+nohup npm run dev 1>/root/kokonut_api_guide/logs/$(date +%Y-%m-%d)_stdout.log 2>/root/kokonut_api_guide/logs/$(date +%Y-%m-%d)_stderr.log &
 
 exit
